be1aa7b9523d708e388eed015ec108217751187e
Eric Wooley <ericwooley@gmail.com>
2021-01-03 13:06:04 -0800

    remember window position

--- src/App.ts
+++ src/App.ts
@@ -7,6 +7,13 @@ import { EventEnum, IdleMonitor } from './IdleMonitor'
 import initTray from './tray'
 import { environment } from './environment'
 import { IElectronSettings, defaultElectronSettings } from './ElectronSettings'
+import isNumber from 'lodash/isNumber'
+const desktopSettingsName = environment.production
+  ? 'tengable-desktop-settings'
+  : 'tengable-desktop-settings-dev'
+const windowSettingsName = environment.production
+  ? 'tengable-window-settings'
+  : 'tengable-window-settings-dev'
 export default class App {
   // Keep a global reference of the window object, if you don't, the window will
   // be closed automatically when the JavaScript object is garbage collected.
@@ -24,7 +31,14 @@ export default class App {
     this.mainWindow?.webContents.send('idle', false)
   }
   store = new Store({
-    name: 'tengable-desktop-settings',
+    name: desktopSettingsName,
+  })
+  windowStore = new Store({
+    name: windowSettingsName,
+    defaults: {
+      position: [] as number[],
+      size: [] as number[],
+    },
   })
   private readonly toggleWindowVisibility = (): void => {
     if (this.mainWindow && !this.mainWindow.isVisible()) {
@@ -108,19 +122,11 @@ export default class App {
   }
 
   initMainWindow() {
-    const width = 412
-    const height = 732
-    // Create the browser window.
-    this.mainWindow = new BrowserWindow({
+    let options: Electron.BrowserWindowConstructorOptions = {
       title: 'Tengable',
       icon: join(__dirname, 'assets', 'icon.png'),
       frame: false,
-      // movable: false,
-      width: width,
-      height: height,
-      // resizable: false,
       alwaysOnTop: false,
-      // show: false,
       webPreferences: {
         // TODO: switch to https://www.electronjs.org/docs/api/context-bridge
         contextIsolation: false,
@@ -128,7 +134,29 @@ export default class App {
         nodeIntegration: false,
         backgroundThrottling: false,
       },
-    })
+    }
+    const [width, height] = this.windowStore.get('size')
+    const [x, y] = this.windowStore.get('position')
+    if (isNumber(width) && isNumber(height)) {
+      options = { ...options, width, height }
+    }
+    if (isNumber(x) && isNumber(y)) {
+      options = { ...options, x, y }
+    }
+    console.log('options', options)
+    // Create the browser window.
+    this.mainWindow = new BrowserWindow(options)
+    // over ride random os placements or whatever
+    let startupPositionerInterval = setInterval(() => {
+      if (isNumber(x) && isNumber(y)) this.mainWindow?.setPosition(x, y)
+      if (isNumber(width) && isNumber(height))
+        this.mainWindow?.setSize(width, height, false)
+    }, 50)
+    setTimeout(() => {
+      clearInterval(startupPositionerInterval)
+    }, 50)
+    this.mainWindow.on('move', this.updateWindowSettings)
+    this.mainWindow.on('resize', this.updateWindowSettings)
     ipcMain.on('listening-for-events', () => {
       ipcMain.addListener('open-dev-tools', () => {
         console.log('open dev tools')
@@ -146,7 +174,6 @@ export default class App {
       e.preventDefault()
       this.mainWindow?.hide()
     })
-    // this.mainWindow.webContents.openDevTools({ mode: 'detach' })
     this.mainWindow.setMenu(null)
     this.mainWindow.center()
     console.log('app path', app.getAppPath())
@@ -166,6 +193,7 @@ export default class App {
     this.mainWindow.webContents.on('will-navigate', this.onRedirect)
     this.mainWindow.webContents.on(
       'new-window',
+
       (event, url, frameName, disposition, options) => {
         this.onRedirect(event, url)
       }
@@ -173,9 +201,6 @@ export default class App {
 
     // Emitted when the window is closed.
     this.mainWindow.on('closed', () => {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
       this.mainWindow = null
       console.log('main window closed')
     })
@@ -225,6 +250,13 @@ export default class App {
     console.log(typeof parsedSettings)
     this.store.set('settings', JSON.stringify({ ...parsedSettings }))
     this.applySettings(parsedSettings)
+  }
+  updateWindowSettings = () => {
+    const position = this.mainWindow?.getPosition()
+    const size = this.mainWindow?.getSize()
+    this.windowStore.set('position', position)
+    this.windowStore.set('size', size)
+    // done
   }
   applySettings = async (settings: IElectronSettings) => {
     console.log('applying settings', settings, typeof settings)
@@ -278,6 +310,7 @@ export default class App {
     console.log('userData location', app.getPath('userData'))
     this.BrowserWindow = browserWindow
     this.application = app
+
     console.log('Tengable version: ', app.getVersion())
     // this.application.on('window-all-closed', this.onWindowAllClosed) // Quit when all windows are closed.
     this.application.on('ready', this.onReady) // App is ready to load data
